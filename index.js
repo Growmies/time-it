d145279f7e2dba32529e124244dd0ceb1efaa15c
Scott Hillman <hillmanov+github@gmail.com>
2015-11-11 11:10:27 -0700

    Multi instance support

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 var util = require('util');
 
-var TimeIt = {
-  timers: {},
-
-  time: function(label) {
-    label = label || '__default';
-    this.timers[label] = process.hrtime();
-    return;
-  },
-
-  timeEnd: function(label, raw) {
-    label = label || '__default';
-    if (!this.timers[label]) return;
-
-    var duration     = process.hrtime(this.timers[label]);
-    var seconds      = duration[0];
-    var milliseconds = duration[1]/1000000;
-
-    delete this.timers[label];
-    if (raw) {
-      return seconds + milliseconds / 1000;
-    }
-    return util.format("%ds %dms", seconds, milliseconds);
-  }
+function TimeIt() {
+  this.timers = {};
 };
+
+TimeIt.prototype.time = function(label) {
+  label = label || '__default';
+  this.timers[label] = process.hrtime();
+  return;
+},
+
+TimeIt.prototype.timeEnd = function(label, raw) {
+  label = label || '__default';
+  if (!this.timers[label]) return;
+
+  var duration     = process.hrtime(this.timers[label]);
+  var seconds      = duration[0];
+  var milliseconds = duration[1]/1000000;
+
+  delete this.timers[label];
+  if (raw) {
+    return seconds + milliseconds / 1000;
+  }
+  return util.format("%ds %dms", seconds, milliseconds);
+}
 
 module.exports = TimeIt;
--- test/index.js
+++ test/index.js
@@ -1,25 +1,29 @@
 var TimeIt = require('../');
 var expect = require('chai').expect;
 
+
 describe('Test TimeIt', function() {
   it('Should time things accurately', function(done) {
-    TimeIt.time('test');
+    var timeIt = new TimeIt();
+    timeIt.time('test');
     setTimeout(function() {
-      var duration = TimeIt.timeEnd('test', true);
+      var duration = timeIt.timeEnd('test', true);
       expect(duration).to.be.above(0.050).and.to.be.below(0.056);
       done();
     }, 50);
   });
 
   it('Should track several timers at the same time', function(done) {
+    timeIt = new TimeIt();
+
     var iAmDone = after(100, done);
 
     for (var i = 0; i < 100; i++) {
       (function(i) {
         var label = 'test-' + i;
-        TimeIt.time(label);
+        timeIt.time(label);
         setTimeout(function() {
-          var duration = TimeIt.timeEnd(label, true);
+          var duration = timeIt.timeEnd(label, true);
           var min = (i / 1000) - 0.005;
           var max = (i / 1000) + 0.005;
           expect(duration).to.be.above(min).and.to.be.below(max);
@@ -27,6 +31,30 @@ describe('Test TimeIt', function() {
         }, i);
       })(i);
     };
+  });
+
+  it('Should support different instances without label clashing', function(done) {
+    timeIt1 = new TimeIt();
+    timeIt2 = new TimeIt();
+
+    timeIt1.time('timer');
+    timeIt2.time('timer');
+
+    setTimeout(function() {
+      var duration = timeIt1.timeEnd('timer', true);
+      var min = (10 / 1000) - 0.005;
+      var max = (10 / 1000) + 0.005;
+      expect(duration).to.be.above(min).and.to.be.below(max);
+    }, 10);
+
+    setTimeout(function() {
+      var duration = timeIt2.timeEnd('timer', true);
+      var min = (50 / 1000) - 0.005;
+      var max = (50 / 1000) + 0.005;
+      expect(duration).to.be.above(min).and.to.be.below(max);
+      done();
+    }, 50);
+
   });
 });
 
